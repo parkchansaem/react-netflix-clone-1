3713915360a56fd96b28715a875851bfc25eb4e5
GW <kowonp@gmail.com>
2021-12-28 23:57:47 +0900

    Slider Part one, two

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { handleMovieNowPlaying, IMovieNowPlaying } from "../api";
@@ -34,9 +36,60 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ backdroppath: string }>`
+  height: 200px;
+  font-size: 26px;
+  background: url(${(props) => props.backdroppath}) no-repeat center center;
+  background-size: cover;
+`;
+
+const rowVariants = {
+  initial: { x: window.outerWidth + 10 },
+  animate: { x: 0, transition: { type: "tween", duration: 1 } },
+  exit: { x: -window.outerWidth - 10, transition: { type: "tween", duration: 1 } },
+};
+
+const numberOfMovie = 6;
+
 const Home = () => {
   const { isLoading, data } = useQuery<IMovieNowPlaying | undefined>(["movies", "nowPlaying"], handleMovieNowPlaying);
   console.log("isLoading, data", isLoading, data);
+
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+
+  const handleToggleLeaving = () => {
+    setLeaving((current) => !current);
+  };
+
+  const handleIncreaseIndex = () => {
+    if (leaving) {
+      return;
+    }
+
+    if (data) {
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / numberOfMovie) - 1;
+      console.log("totalMovies", totalMovies);
+      console.log("maxIndex", maxIndex);
+
+      handleToggleLeaving();
+      setIndex((currentIndex) => (currentIndex === maxIndex ? 0 : currentIndex + 1));
+    }
+  };
 
   return (
     <Wrapper>
@@ -44,10 +97,22 @@ const Home = () => {
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner backdropPath={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner onClick={handleIncreaseIndex} backdropPath={makeImagePath(data?.results[0].backdrop_path || "")}>
             <Title>{data?.results[0]?.title}</Title>
             <Overview>{data?.results[0]?.overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={handleToggleLeaving}>
+              <Row key={index} variants={rowVariants} initial="initial" animate="animate" exit="exit">
+                {data?.results
+                  ?.slice(1)
+                  .slice(numberOfMovie * index, numberOfMovie * index + numberOfMovie)
+                  .map((movie) => (
+                    <Box key={movie.id} backdroppath={makeImagePath(movie.backdrop_path, "w500")}></Box>
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
